b7d62f4b7f32eaab4067cf5fdfa0d48fe265890a
i-oliveira <50674376+i-oliveira@users.noreply.github.com>
2019-06-27 18:32:02 -0300

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,11 +1,9 @@
 <?php  
-// CONSTANTES DE CONEXÃO
 define('SERVER', 'localhost');
 define('USER', 'root');
 define('PASSWORD', '');
 define('DB', 'bd_session');
 
-// conectar: neste servidor, com este usuario, esta senha, neste banco de dados
 $conn = mysqli_connect(SERVER, USER, PASSWORD, DB);
 
 ?>
